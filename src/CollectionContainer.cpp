fef3aa22ea5b3363d038a84cf32e269aabe8dbab
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-03-16 21:04:43 +0700

    memperbaiki cctor dan ctor di collectioncontainer

--- src/CollectionContainer.cpp
+++ src/CollectionContainer.cpp
@@ -2,21 +2,39 @@
 
 CollectionContainer::CollectionContainer()
 {
-    this -> mxCol = 0;
-    this -> mxRow = 0;
+    this->mxCol = 0;
+    this->mxRow = 0;
 }
 
 CollectionContainer::CollectionContainer(int mxRow, int mxCol)
 {
-    this -> mxRow = mxRow;
-    this -> mxCol = mxCol;
+    this->mxRow = mxRow;
+    this->mxCol = mxCol;
+    for (int i = 0; i < this->mxRow; i++)
+    {
+        vector<Slot> s;
+        for (int j = 0; j < this->mxCol; j++)
+        {
+            s.push_back(Slot());
+        }
+        container.push_back(s);
+    }
 }
 
 // copy constructor
 CollectionContainer::CollectionContainer(const CollectionContainer &cc)
 {
-    this -> mxRow = cc.mxRow;
-    this -> mxCol = cc.mxCol;
+    this->mxRow = cc.mxRow;
+    this->mxCol = cc.mxCol;
+    for (int i = 0; i < this->mxRow; i++)
+    {
+        vector<Slot> s;
+        for (int j = 0; j < this->mxCol; j++)
+        {
+            s.push_back(cc.container[i][j]);
+        }
+        container.push_back(s);
+    }
 }
 
 // destructor
@@ -27,24 +45,39 @@ CollectionContainer::~CollectionContainer()
 // operator
 CollectionContainer &CollectionContainer::operator=(CollectionContainer &cc)
 {
-    CollectionContainer(cc);
+    this->container.clear();
+    this->mxRow = cc.mxRow;
+    this->mxCol = cc.mxCol;
+    for (int i = 0; i < this->mxRow; i++)
+    {
+        vector<Slot> s;
+        for (int j = 0; j < this->mxCol; j++)
+        {
+            s.push_back(cc.container[i][j]);
+        }
+        container.push_back(s);
+    }
 }
 
 void CollectionContainer::insertItem(Position p, Item *item)
 {
-    if (this -> container[p.row][p.col].full()) {
+    if (this->container[p.row][p.col].full())
+    {
         // exception
-    } else {
-        
+    }
+    else
+    {
     }
 }
 
 void CollectionContainer::deleteItem(Position p)
 {
-    if (this -> container[p.row][p.col].empty()) {
+    if (this->container[p.row][p.col].empty())
+    {
         // exception
-    } else {
-       
+    }
+    else
+    {
     }
 }
 
