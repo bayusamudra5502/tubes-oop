931e901b437ac58fa5a7b1959f8ce6ae9cd64ba8
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-03-16 18:54:33 +0700

    collectioncontainer, belum final

---
+++ src/CollectionContainer.cpp
@@ -0,0 +1,36 @@
+#include <CollectionContainer.hpp>
+
+CollectionContainer::CollectionContainer()
+{
+}
+
+CollectionContainer::CollectionContainer(int mxRow, int mxCol)
+{
+}
+
+// copy constructor
+CollectionContainer::CollectionContainer(const CollectionContainer &cc)
+{
+}
+
+// destructor
+CollectionContainer::~CollectionContainer()
+{
+}
+
+// operator
+CollectionContainer &CollectionContainer::operator=(CollectionContainer &cc)
+{
+}
+
+void CollectionContainer::insertItem(int idxPosisi, Item *Item)
+{
+}
+
+void CollectionContainer::deleteItem(int idxPosisi)
+{
+}
+
+void CollectionContainer::getIterator(int idxPosisi)
+{
+}
--- src/headers/CollectionContainer.hpp
+++ src/headers/CollectionContainer.hpp
@@ -1,14 +1,27 @@
 #pragma once
 #include <Slot.hpp>
 
-class CollectionContainer {
-    private:
-        vector<vector<Slot>> container;
-        int mxRow; // banyak baris
-        int mxCol; // banyak kolom
-    public:
-        CollectionContainer(int mxRow, int mxCol);
-        void insertItem(int idxPosisi, Item* Item);
-        void deleteItem(int idxPosisi);
-        void getIterator(int idxPosisi);
+class CollectionContainer
+{
+private:
+    vector<vector<Slot>> container;
+    int mxRow; // banyak baris
+    int mxCol; // banyak kolom
+public:
+    // constructor
+    CollectionContainer();
+    CollectionContainer(int mxRow, int mxCol);
+
+    // copy constructor
+    CollectionContainer(const CollectionContainer &cc);
+
+    // destructor
+    ~CollectionContainer();
+    // operator
+    CollectionContainer &operator=(CollectionContainer &cc);
+
+    void insertItem(int idxPosisi, Item *Item);
+    void deleteItem(int idxPosisi);
+    void getIterator(int idxPosisi);
+    //friend ofstream &operator<<(ofstream &stream, const CollectionContainer &cc);
 };
