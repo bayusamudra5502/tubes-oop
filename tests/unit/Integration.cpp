79b2a5a82ad507488c1a589678e2b73420792f6d
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-03-24 19:25:46 +0700

    Irhamna ya Robb

---
+++ tests/unit/Integration.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include <Item.hpp>
+#include <Map.hpp>
+#include <NonToolItem.hpp>
+#include <Slot.hpp>
+#include <ToolItem.hpp>
+#include <vector>
+
+using namespace std;
+
+TEST(ITEM_DICTIONARY, UJI_DICTIONARY) {
+  vector<Slot> vc;
+  Map<string, Item*> mi;
+
+  mi.insertItem("Belalang", new NonToolItem(1, "Belalang", "Hewan"));
+  mi.insertItem("Kucing", new NonToolItem(2, "Kucing", "Hewan"));
+  mi.insertItem("Hewan", new NonToolItem(-2, "*", "Hewan"));
+  mi.insertItem("Mobil", new NonToolItem(3, "Mobil", "Kendaraan"));
+  mi.insertItem("Sepeda", new NonToolItem(4, "Sepeda", "Kendaraan"));
+  mi.insertItem("Kendaraan", new NonToolItem(-2, "*", "Kendaraan"));
+
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(1, mi["Belalang"])));
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(2, mi["Kucing"])));
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(3, mi["Hewan"])));
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(4, mi["Mobil"])));
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(4, mi["Sepeda"])));
+  ASSERT_NO_FATAL_FAILURE(vc.push_back(Slot(5, mi["Kendaraan"])));
+}
